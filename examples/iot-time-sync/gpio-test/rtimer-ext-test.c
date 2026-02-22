a0fb4ea34deaa7547fa0d7021578c077a93d3381
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2026-02-22 21:47:01 +0800

    Add test codes for rtimer 32-bit extension.

---
+++ examples/iot-time-sync/gpio-test/rtimer-ext-test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <isr_compat.h>
+#include "contiki.h"
+#include "dev/button-sensor.h"
+#include "dev/leds.h"
+#include "sys/rtimer.h"
+
+PROCESS(rtimer_ext_process, "32-Bit Rtimer Extension");
+AUTOSTART_PROCESSES(&rtimer_ext_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(rtimer_ext_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  const int period = 0.1 * CLOCK_SECOND;
+  static struct etimer et;
+  static rtimer_ext_clock_t now = 0, prev = 0;
+  static int i;
+
+  etimer_set(&et, 5*CLOCK_SECOND); // initial delay
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+  printf("\n\n\n");
+  printf("# Defined macros:\n");
+#ifdef RTIMER_EXT
+  printf("# - RTIMER_EXT: For 32-bit extension\n");
+#endif
+  printf("##### BEGIN\n"); 
+  printf("t,diff\n");
+  prev = RTIMER_EXT_NOW();
+  for (i = 0; i < 1000; i++) {
+      etimer_set(&et, period);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      now = RTIMER_EXT_NOW();
+      printf("%"RTIMER_PRI_EXT",%"RTIMER_PRI_EXT"\n", now, now-prev);
+      prev = now;
+  }
+  printf("##### END\n");
+  
+  PROCESS_END();
+}
---
+++ examples/iot-time-sync/gpio-test/rtimer-ext2.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include "contiki.h"
+#include "dev/button-sensor.h"
+#include "dev/leds.h"
+#include "sys/rtimer.h"
+
+/* Define the bit mask for P2.X */
+// #define P2_PIN  (1 << P2_EXT)
+
+/* Link to the variables in button-sensor.c */
+extern rtimer_ext_clock_t gio_timestamp;
+extern volatile uint8_t gio_triggered;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(gpio_trigger_process, "GPIO Trigger Process");
+struct process *p2_ext_process = &gpio_trigger_process; // for the button-sensor extension
+AUTOSTART_PROCESSES(&gpio_trigger_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(gpio_trigger_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  /* 1. ACTIVATE BUTTON (The Probe) */
+  SENSORS_ACTIVATE(button_sensor);
+
+  // /* 2. CONFIGURE P2_PIN (The Jumper) */
+  // P2SEL &= ~P2_PIN; // as GPIO
+  // P2DIR &= ~P2_PIN; // as input
+  // P2IES &= ~P2_PIN; // rising edge
+  // P2IE  |= P2_PIN;  // enable interrupt
+  // P2IFG &= ~P2_PIN; // clear any initial noise
+
+  printf("TelosB Sync App Started.\n");
+  printf("Hold Jumper to VCC and press Button to inspect P2IFG.\n");
+
+  /* Read-back for Debugging */
+  printf("--- Hardware Debug Check ---\n");
+  printf("P2DIR: 0x%02X (Should have bit %d as 0)\n", P2DIR, P2_EXT);
+  printf("P2SEL: 0x%02X (Should have bit %d as 0)\n", P2SEL, P2_EXT);
+  printf("P2IES: 0x%02X (Should have bit %d as 0 for Rising Edge)\n", P2IES, P2_EXT);
+  printf("P2IE:  0x%02X (Bit %d MUST be 1)\n", P2IE, P2_EXT);
+  printf("P2IN:  0x%02X (Current logic levels on all Port 2 pins)\n", P2IN);  
+  printf("--- Hardware Debug Check ---\n");
+  printf("TelosB PORT 2.%d initialized.\n", P2_EXT);
+  
+  while(1) {
+    PROCESS_WAIT_EVENT();
+
+    /* CASE A: Our Pin 6 actually worked! */
+    if(ev == PROCESS_EVENT_POLL && gio_triggered == 1) {
+      printf(">>> SUCCESS: P2.%d (Jumper) Triggered the ISR directly!\n", P2_EXT);
+#ifdef RTIMER_EXT
+      printf("Captured 32-bit timestamp: %10"RTIMER_PRI_EXT"\n", gio_timestamp);
+#else
+      printf("Captured 16-bit timestamp: %5"RTIMER_PRI_EXT"\no", gio_timestamp);
+#endif
+      // printf("Captured P2IFG: 0x%02X\n", debug_flags);
+      // printf("Captured P2SEL: 0x%02X\n", debug_sel);
+      // printf("Captured P2DIR: 0x%02X\n", debug_dir);
+      gio_triggered = 0;
+      // leds_off(LEDS_RED);
+    }
+
+    /* CASE B: The Button was pressed (The Probe) */
+    if(ev == sensors_event && data == &button_sensor) {
+      // printf("\n--- PORT 2 CRIME SCENE CAPTURE ---\n");
+      // printf("P2IFG: 0x%02X\n", debug_flags);
+      // printf("P2SEL: 0x%02X\n", debug_sel);
+      // printf("P2DIR: 0x%02X\n", debug_dir);
+      
+      // if(!(debug_flags & 0x40)) {
+      //   printf("RESULT: Bit 6 is MISSING in hardware. Forcing Software Trigger Test...\n");
+        
+      //   /* THE NUCLEAR TEST: Manually set the flag */
+      //   P2IFG |= 0x40; 
+      //   printf("Action: P2IFG |= 0x40 executed. Check if Red LED turned on.\n");
+      // }
+    }
+  }
+
+//       /* Wait for the poll signal from the ISR */
+//     //   PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
+//     //   PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event);
+
+//       printf("PORT2 event detected\n");
+//       printf("P2IE:  0x%02X (Current logic P2IE value)\n", P2IE);
+//       printf("P2IN:  0x%02X (Current logic levels on all Port 2 pins)\n", P2IN);
+      
+//       printf("\n--- PORT 2 STATE CAPTURE ---\n");
+//       printf("P2IFG (Flags): 0x%02X\n", p2_ifg);
+//       printf("P2SEL (Select): 0x%02X\n", p2_sel);
+//       printf("P2DIR (Direction): 0x%02X\n", p2_dir);
+
+//       if(p2_ifg & 0x40) {
+//         printf("Result: Bit 6 Flag IS set!\n");
+//       } else {
+//         printf("Result: Bit 6 Flag is MISSING.\n");
+//       }
+
+//       if (gio_triggered == 1) {
+//           printf("Edge detected on P2.6 at %u\n", (unsigned)gio_timestamp);
+//           leds_toggle(LEDS_GREEN);
+//           printf("P2IE:  0x%02X (Current logic P2IE value)\n", P2IE);
+//           printf("P2IN:  0x%02X (Current logic levels on all Port 2 pins)\n", P2IN);  
+//           gio_triggered = 0; // clear the flag
+
+
+//       }
+//       leds_toggle(LEDS_RED);
+//   }
+
+  PROCESS_END();
+}
