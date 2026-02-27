29f13a0c0c156aa0b8093c3a00c9d16f4e1949fa
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2026-02-27 22:41:51 +0800

    Clean up the code.

--- examples/iot-time-sync/sys-info/sys-info.c
+++ examples/iot-time-sync/sys-info/sys-info.c
@@ -14,30 +14,9 @@
 #include "os/net/linkaddr.h"
 #include "sys/rtimer.h"
 #if defined(CONTIKI_TARGET_SKY)
-#include "dev/sensor/sht11/sht11-sensor.h" // TelosB Temperature/Humidity
-#include "dev/light-sensor.h" // TelosB Light
+#include "dev/sensor/sht11/sht11-sensor.h" // for temperature/humidity sensors
+#include "dev/light-sensor.h"
 #endif
-
-/* unsigned long getSMCLK() { */
-/*     unsigned long sourceFreq; */
-/*     unsigned int divider; */
-
-/*     // Check SELS bit in BCSCTL2 */
-/*     if (BCSCTL2 & SELS) { */
-/*         // Source is XT2 (assuming XT2 is active and used) */
-/*         sourceFreq = 8000000; // Example: 8MHz crystal */
-/*     } else { */
-/*         // Source is DCO */
-/*         // Need to calculate DCO frequency based on DCOCTL/BCSCTL1 */
-/*         sourceFreq = 1048576; // Example default */
-/*     } */
-
-/*     // Determine Divider */
-/*     divider = (BCSCTL2 & DIVS1) ? ((BCSCTL2 & DIVS0) ? 8 : 4) : ((BCSCTL2 & DIVS0) ? 2 : 1); */
-/*     // Alternatively: divider = 1 << ((BCSCTL2 >> 4) & 0x03); */
-
-/*     return sourceFreq / divider; */
-/* } */
 
 PROCESS(sys_info_process, "System information process");
 AUTOSTART_PROCESSES(&sys_info_process);
@@ -80,10 +59,8 @@ PROCESS_THREAD(sys_info_process, ev, data)
     printf("sizeof(long long): %lu bytes\n", (long unsigned)sizeof(long long));
     printf("sizeof(float): %lu bytes\n", (long unsigned)sizeof(float));
     printf("sizeof(double): %lu bytes\n", (long unsigned)sizeof(double));
-    // printf("sizeof(rtimer_clock_t): %lu bytes\n", (long unsigned)(sizeof t1));
     printf("F_CPU: %lu Hz\n", (long unsigned)F_CPU);
     printf("MSP430_CPU_SPEED: %lu Hz\n", (long unsigned)MSP430_CPU_SPEED);
-    /* printf("SMCLK: %lu Hz\n", getSMCLK()); */
     printf("CLOCK_SIZE: %u\n", (unsigned)CLOCK_SIZE);
     printf("CLOCK_SECOND: %lu\n", (long unsigned)CLOCK_SECOND);
     printf("RTIMER_CLOCK_SIZE: %u\n", (unsigned)RTIMER_CLOCK_SIZE);
@@ -91,7 +68,6 @@ PROCESS_THREAD(sys_info_process, ev, data)
     printf("RTIMER_CLOCK_MAX: %lu\n", (long unsigned)RTIMER_CLOCK_MAX);
     printf("BCSCTL2: %x\n", BCSCTL2);
 #if defined(CONTIKI_TARGET_SKY)
-    // test sensors
     int temp = sht11_sensor.value(SHT11_SENSOR_TEMP);
     int light = light_sensor.value(LIGHT_SENSOR_TOTAL_SOLAR);
     printf("Temperature: %d C, Light: %d\n", (temp/10), light);
